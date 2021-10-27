504df394782af366c3bbee974390ab974aeaad79
SinnottTM <tim.sinnott86@gmail.com>
2021-10-27 13:44:59 +0100

    initial commit

---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            int minLength = 2;
+
+            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            string lowercase = "abcdefghijklmnopqrstuvwxyz";
+
+            string digits = "12345678910";
+
+            string specialChars = "!@£$%^&*()";
+
+            Console.WriteLine("Please enter your password: ");
+            string password = Console.ReadLine();
+
+            int score = 0;
+
+            if (password.Length > minLength)
+            {
+                score++;
+            }
+
+            if (password.Contains(uppercase))
+            {
+                score++;
+            }
+
+            if (password.Contains(lowercase))
+            {
+                score++;
+            }
+
+            if (password.Contains(digits))
+            {
+                score++;
+            }
+
+            if (password.Contains(specialChars))
+            {
+                score++;
+            }
+
+            switch (score)
+            {
+                case 5:
+                case 4:
+                    Console.WriteLine("This password is very strong");
+                    break;
+                case 3:
+                    Console.WriteLine("This password is strong");
+                    break;
+                case 2:
+                    Console.WriteLine("This password is medium");
+                    break;
+                case 1:
+                    Console.WriteLine("This password is weak");
+                    break;
+                default:
+                    Console.WriteLine("This is not a password");
+                    break;
+            }
+        }
+    }
+}
